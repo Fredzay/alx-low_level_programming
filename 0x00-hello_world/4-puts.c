51c677710bfbb1f88d0123b221ea19d488f8d409
Fredx <fedrickseyi@gmail.com>
2022-09-09 10:57:06 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,13 +4,12 @@
 /**
  * *main - Main function of my project
  * *
- * *
  * *Return: return 0 everytime
  * */
 int main(void)
 {
   char displayed[52];
-  strcpy(displayed, "\"Programming is like building a multilingual puzzle");
+  strcpy(displayed, "\"Programming is like building a multilingual puzzle")
 	puts(displayed);
   return (0);
 }
